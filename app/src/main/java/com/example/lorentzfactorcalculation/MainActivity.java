14c07ca825ba86ebe918dba449cb77eb44a24094
shivamgup0001 <shivamgup00001@gmail.com>
2021-06-19 01:19:56 +0530

    second commit

--- app/src/main/java/com/example/lorentzfactorcalculation/MainActivity.java
+++ app/src/main/java/com/example/lorentzfactorcalculation/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.lorentzfactorcalculation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.solver.state.State;
+import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputFilter;
 import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.text.method.DigitsKeyListener;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,27 +21,32 @@ import android.widget.Toast;
 import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
 import java.text.ParseException;
-
-public class MainActivity extends AppCompatActivity {
+import android.os.Handler;
+import android.os.Vibrator;
+public class MainActivity extends AppCompatActivity{
     private Button button;
+    private Button button4;
     private EditText editText;
+
     private TextView textView;
     private double result;
     private long a;
+    static ViewGroup layout ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         editText = findViewById(R.id.editText);
         button = findViewById(R.id.button);
         textView = findViewById(R.id.textView);
+         button4=findViewById(R.id.button4);
         long c = 300000000;
-
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view1) {
 
                 if (TextUtils. isEmpty(editText. getText(). toString()))
                     Toast.makeText(MainActivity.this, "Please enter a value", Toast.LENGTH_SHORT).show();
@@ -46,15 +56,27 @@ public class MainActivity extends AppCompatActivity {
                     if (a < c) {
                         double z = Math.pow((Math.pow(c, 2) - Math.pow(a, 2)), 0.5);
                         result = c / z;
-                        textView.setText("The Lorentz factor is " + result);
-                    } else {
+
+                         // Toast.makeText(MainActivity.this, "result "+y, Toast.LENGTH_SHORT).show();
+
+                            textView.setText("Your calculated value of Lorentz factor is \n"+result);
+                        }
+                        else
+                         {
                         Toast.makeText(MainActivity.this, "The value of velocity entered is invalid.", Toast.LENGTH_SHORT).show();
-                    }
+                        }
                 }
             }
+
         });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent (MainActivity.this,MainActivity2.class);
+                startActivity(i);
 
-
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/example/lorentzfactorcalculation/MainActivity2.java
@@ -0,0 +1,93 @@
+package com.example.lorentzfactorcalculation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Context;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.os.Handler;
+import android.os.Vibrator;
+
+
+public class MainActivity2 extends AppCompatActivity {
+    private Button button2;
+    private EditText editText4;
+    private EditText editText3;
+    private TextView textView2;
+    private double result1;
+    private long a1;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+     //  ViewGroup layout = (ViewGroup) view.findViewById(R.id.widget101);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        editText3 =findViewById(R.id.editText3);
+        button2 = findViewById(R.id.button2);
+        textView2 = findViewById(R.id.textView2);
+        editText4 = findViewById(R.id.editText4);
+        long c = 300000000;
+        View view = this.getWindow().getDecorView();
+        final Handler handler = new Handler();
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view1) {
+
+                if (TextUtils. isEmpty(editText3. getText(). toString()))
+                    Toast.makeText(MainActivity2.this, "Please enter a value", Toast.LENGTH_SHORT).show();
+                else {
+                    String s =editText3.getText().toString();
+                    String s1 =editText4.getText().toString();
+                    long a1 = Long.parseLong(s);
+                    if (a1 < c) {
+                        double z = Math.pow((Math.pow(c, 2) - Math.pow(a1, 2)), 0.5);
+                        result1 = c / z;
+
+                        double y = Double.parseDouble(s1);
+                        // Toast.makeText(MainActivity.this, "result "+y, Toast.LENGTH_SHORT).show();
+                        if (y == result1) {
+                            Toast.makeText(MainActivity2.this, "Your answer is right", Toast.LENGTH_SHORT).show();
+                            textView2.setText("Your answer is correct ");
+                            view.setBackgroundResource(R.color.green);
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    view.setBackgroundResource(R.color.white);
+                                }
+                            }, 2000);
+
+                        } else {
+                            Toast.makeText(MainActivity2.this, "Your answer is wrong", Toast.LENGTH_SHORT).show();
+                            v.vibrate(400);
+                            textView2.setText("Your answer is wrong\nThe correct answer is\n" + result1);
+                            view.setBackgroundResource(R.color.red);
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    view.setBackgroundResource(R.color.white);
+                                }
+                            }, 2000);
+
+                        }
+                    }
+
+                    else
+                    {
+                        Toast.makeText(MainActivity2.this, "The value of velocity entered is invalid.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+
+    }
+
+}
